a5262cb26f9b00576d43c7e564dc2d362edb9570
doublexinori <598439233@qq.com>
2018-01-11 10:36:40 +0800

    修改问题。

--- suzhoumeng.py
+++ suzhoumeng.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
-import json
 import collections
 
 
@@ -13,37 +12,50 @@ def in_mongo(data):
     print('One post: {0}'.format(result.inserted_id))
 
 
-year = 2017
-month = 1
+year = 2015
+month = 8
 go = True
 while go:
+    if month == 13:
+        year += 1
+        month = 1
     dr = webdriver.PhantomJS()
-    dr.get('http://newhouse.suzhou.fang.com/top/cjdetail-%d-%d' % (year, month))
+    dr.get('http://newhouse.suzhou.fang.com/top/xtdetail-%d-%d' % (year, month))
     html = dr.page_source
     soup = BeautifulSoup(html, 'html.parser')
-    if soup.find('ul', attrs={'id': 'cjDetail'}) is not None:
-        divs = soup.find('ul', attrs={'id': 'cjDetail'}).find_all('li')
-        for div in divs:
-            data = collections.OrderedDict()
-            data['date'] = '%d-%d' % (year, month)
-            data['location'] = div.find('div', attrs={'class': 'text'}).find('span').get_text()
-            data['name'] = div.find('div', attrs={'class': 'text'}).find('a').get_text()
-            data['price'] = div.find('div', attrs={'class': 'price'}).find('span').get_text()
-            in_mongo(data)
-    else:
+    if '%d%d' % (year, month) == '20181':
         dr.close()
         dr = webdriver.PhantomJS()
-        dr.get('http://newhouse.suzhou.fang.com/top/cjdetail')
+        dr.get('http://newhouse.suzhou.fang.com/top/xtdetail')
         html = dr.page_source
         soup = BeautifulSoup(html, 'html.parser')
-        divs = soup.find('ul', attrs={'id': 'cjDetail'}).find_all('li')
+        divs = soup.find('ul', attrs={'id': 'xtdetailbox'}).find_all('li')
         for div in divs:
             data = collections.OrderedDict()
             data['date'] = '%d-%d' % (year, month)
             data['location'] = div.find('div', attrs={'class': 'text'}).find('span').get_text()
             data['name'] = div.find('div', attrs={'class': 'text'}).find('a').get_text()
-            data['price'] = div.find('div', attrs={'class': 'price'}).find('span').get_text()
+            if div.find('div', attrs={'class': 'price'}).find('span').get_text() == '价格待定':
+                continue
+            if len(div.find('div', attrs={'class': 'price'}).find('span').get_text()) <= 3:
+                data['price'] = div.find('div', attrs={'class': 'price'}).find('span').get_text() + '万元起'
+            else:
+                data['price'] = div.find('div', attrs={'class': 'price'}).find('span').get_text() + '元/平方米'
             in_mongo(data)
         go = False
+    if soup.find('ul', attrs={'id': 'xtdetailbox'}) is not None:
+        divs = soup.find('ul', attrs={'id': 'xtdetailbox'}).find_all('li')
+        for div in divs:
+            data = collections.OrderedDict()
+            data['date'] = '%d-%d' % (year, month)
+            data['location'] = div.find('div', attrs={'class': 'text'}).find('span').get_text()
+            data['name'] = div.find('div', attrs={'class': 'text'}).find('a').get_text()
+            if div.find('div', attrs={'class': 'price'}).find('span').get_text() == '价格待定':
+                continue
+            if len(div.find('div', attrs={'class': 'price'}).find('span').get_text()) <= 3:
+                data['price'] = div.find('div', attrs={'class': 'price'}).find('span').get_text() + '万元起'
+            else:
+                data['price'] = div.find('div', attrs={'class': 'price'}).find('span').get_text() + '元/平方米'
+            in_mongo(data)
     month += 1
-    dr.close()
+dr.close()
